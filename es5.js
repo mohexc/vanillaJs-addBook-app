454f59457d3599e0c0317dfc137af64f41e2c41a
nanut <galunyu@gmail.com>
2019-12-21 09:44:50 +0700

    first commit

---
+++ es5.js
@@ -0,0 +1,79 @@
+//Book Constructor
+function Book(title, author, isbn) {
+  this.title = title
+  this.author = author
+  this.isbn = isbn
+
+}
+
+// UI Constructor
+function UI() { }
+
+// Add Book To list
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById('book-list')
+  const row = document.createElement('tr')
+
+  row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href="#!" class = "delete">X</a> </td>
+  `
+  list.appendChild(row)
+}
+
+UI.prototype.clearFields = function () {
+  document.getElementById('title').value = ''
+  document.getElementById('author').value = ''
+  document.getElementById('isbn').value = ''
+}
+
+UI.prototype.showAlert = function (message, className) {
+  const div = document.createElement('div')
+  div.className = `alert ${className}`
+  div.appendChild(document.createTextNode(message))
+  const container = document.querySelector('.container')
+  const form = document.querySelector('#book-form')
+  container.insertBefore(div, form)
+  setTimeout(() => {
+    document.querySelector('.alert').remove()
+  }, 3000)
+}
+
+UI.prototype.deleteBook = function (target) {
+  if (target.className === "delete")
+    target.parentElement.parentElement.remove()
+}
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  const title = document.getElementById('title').value
+  const author = document.getElementById('author').value
+  const isbn = document.getElementById('isbn').value
+
+  const book = new Book(title, author, isbn)
+
+  const ui = new UI()
+
+  if (title === '' || author === "" || isbn === "") {
+    ui.showAlert("Please fill in all field", "error")
+  }
+  else {
+    ui.addBookToList(book)
+    ui.showAlert(`Book Added!`, `success`)
+    ui.clearFields()
+  }
+})
+
+// DELETE
+document.getElementById('book-list').addEventListener('click', (e) => {
+  e.preventDefault()
+
+  const ui = new UI()
+
+  ui.deleteBook(e.target)
+  ui.showAlert("Book Removed!", 'success')
+
+})
